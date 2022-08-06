36f47c63ccfdad123c2c638df5a98820a0d356af
이지영 <wldudwldhd@naver.com>
2022-08-06 21:32:36 +0900

    first commit

---
+++ shop/admin.py
@@ -0,0 +1,18 @@
+from django.contrib import admin
+from .models import Category, Product
+from .models import Product
+
+# Register your models here.
+class CategoryAdmin(admin.ModelAdmin) :
+    list_display = ['name', 'slug']
+    prepopulated_fields = {'slug' : ('name',)}
+
+admin.site.register(Category, CategoryAdmin)
+
+class ProductAdmin(admin.ModelAdmin) :
+    list_display = ['name', 'price']
+    list_editable = ['price',]
+    prepopulated_fields = {'slug' : ('name',)}
+    list_per_page = 20
+
+admin.site.register(Product, ProductAdmin)
