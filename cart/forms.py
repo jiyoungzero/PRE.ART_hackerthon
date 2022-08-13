61635ccdc34faf39352568a250f095e486bd3014
Seulgigi <slki1205@naver.com>
2022-08-14 08:36:13 +0900

    form css

--- cart/forms.py
+++ cart/forms.py
@@ -5,50 +5,57 @@ class PostForm(forms.ModelForm):
     realname = forms.CharField(
         label = '작가 본명',
         widget = forms.TextInput(
-            attrs = {'placeholder':'작가 본명'}
+            attrs = {
+                'placeholder':'작가 본명'}
         ),
         required=True,
     )
     artist_name = forms.CharField(
         label = '작가 활동명',
         widget = forms.TextInput(
-            attrs = {'placeholder':'작가 활동명'}
+            attrs = {
+                'placeholder':'작가 활동명'}
         ),
         required=True,
     )
     team = forms.CharField(
         label = '작가 소속',
         widget = forms.TextInput(
-            attrs = {'placeholder':'작가 소속'}
+            attrs = {
+                'placeholder':'작가 소속'}
         ),
         required=True,
     )
     email = forms.EmailField(
         label = '작가 이메일',
         widget = forms.EmailInput(
-            attrs = {'placeholder':'작가 이메일'}
+            attrs = {
+                'placeholder':'작가 이메일'}
         ),
         required=True,
     )
     artist_intro = forms.CharField(
         label = '작가 한줄 소개',
         widget = forms.TextInput(
-            attrs = {'placeholder':'작가 한줄 소개'}
+            attrs = {
+                'placeholder':'작가 한줄 소개'}
         ),
         required=True,
     )
     post_intro = forms.CharField(
         label = '전시 한줄 소개',
         widget = forms.TextInput(
-            attrs = {'placeholder':'전시 한줄 소개'}
-        ),
+            attrs = {
+                'placeholder':'전시 한줄 소개'}
+                ),
         required=True,
     )
     post_plan = forms.CharField(
         label = '전시 기획 의도',
         widget = forms.TextInput(
-            attrs = {'placeholder':'전시 기획 의도'}
-        ),
+            attrs = {
+                'placeholder':'전시 기획 의도'}
+                ),
         required=True,
     )
 
