dcfff5f3cd0a2e5aeb505b548670190c3ea03cc4
kmjenny <kjn3008@gmail.com>
2022-08-17 15:50:33 +0900

    왜이래

--- cart/admin.py
+++ cart/admin.py
@@ -1,8 +1,14 @@
 from django.contrib import admin
-from .models import Post
+from .models import Post, PostImage
 
 # Register your models here.
 
-@admin.register(Post)
+# @admin.register(Post, PostAdmin)
+class PhotoInline(admin.TabularInline):
+    model = PostImage
+
 class PostAdmin(admin.ModelAdmin):
+    inlines = [PhotoInline, ]
     list_display = ('realname', 'artist_name', 'team', 'email', 'artist_intro', 'post_intro', 'post_plan', 'option')
+
+admin.site.register(Post, PostAdmin)
